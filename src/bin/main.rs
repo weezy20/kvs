b6d0c902de61db8ee41e2396a7d652a763e4f3c5
Abhishek Shah <abhishekshah3@gmail.com>
2023-02-24 19:00:11 +0530

    clap parser using ChatGPT

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,36 +4,44 @@
 #[command(author, version, about)]
 pub struct KvsCLI {
     #[command(subcommand)]
-    action: Option<Subcommand>,
+    action: Option<Action>,
 
     /// Turn debugging information on
     #[arg(short, long, action = clap::ArgAction::Count)]
     debug: u8,
 }
 
+#[derive(clap::Args)]
+/// Set new value at key
+pub struct SetCmd {
+    #[clap(name = "KEY", help = "Key to be set")]
+    key: String,
+    #[clap(name = "VALUE", help = "Value to be set")]
+    value: String,
+}
+
+#[derive(clap::Parser)]
+/// Get value at key
+pub struct GetCmd {
+    #[clap(name = "KEY", help = "Key to be fetch")]
+    key: String,
+}
+
+#[derive(clap::Parser)]
+/// Remove value at key
+pub struct RmCmd {
+    #[clap(name = "KEY", help = "Key to be remove")]
+    pub key: String,
+}
+
 #[derive(clap::Subcommand)]
-pub enum Subcommand {
+pub enum Action {
     /// Set new value at key
-    Set {
-        /// set key
-        #[arg(long)]
-        key: String,
-        /// value
-        #[arg(long)]
-        value: String,
-    },
+    Set(SetCmd),
     /// Get value at key
-    Get {
-        /// set key
-        #[arg(long)]
-        key: String,
-    },
+    Get(GetCmd),
     /// Remove value at key
-    Rm {
-        /// set key
-        #[arg(long)]
-        key: String,
-    },
+    Rm(RmCmd),
 }
 
 fn main() {
