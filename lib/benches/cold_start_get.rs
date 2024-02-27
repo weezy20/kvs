c8b4b3f93152cad5c053baec4ee9f95bf7704b7b
Abhishek Shah <abhishekshah3@gmail.com>
2024-02-27 15:03:30 +0530

    get bench group. work on set

--- lib/benches/cold_start_get.rs
+++ lib/benches/cold_start_get.rs
@@ -1,26 +1,56 @@
 use criterion::{black_box, criterion_group, criterion_main, Bencher, Criterion};
-use kvs::{KvStore, SledKvsEngine, KvsEngine};
+use kvs::{KvStore, KvsEngine, SledKvsEngine};
 use tempfile::TempDir;
 
 fn cold_start_get(c: &mut Criterion) {
-    let temp_dir = TempDir::new().unwrap();
-    c.bench_function("kvs: get key", |b: &mut Bencher<_>| {
+    let mut group = c.benchmark_group("GET");
+    group.bench_function("kvs: get key", |b: &mut Bencher<_>| {
+        let temp_dir = TempDir::new().unwrap();
         let mut store = KvStore::open(&temp_dir.path()).unwrap();
-        store.set("key".to_string(), "some_get_val".to_string());
+        store
+            .set("key".to_string(), "some_get_val".to_string())
+            .unwrap();
+
         b.iter(|| {
-            store.get("key".to_string());
+            let _ = store.get("key".to_string()).unwrap();
         })
     });
-    c.bench_function("sled: get key", |b: &mut Bencher<_>| {
+    group.bench_function("sled: get key", |b: &mut Bencher<_>| {
+        let temp_dir = TempDir::new().unwrap();
         let mut store = SledKvsEngine::open(&temp_dir.path()).unwrap();
-        store.set("key".to_string(), "some_get_val".to_string());
+        store
+            .set("key".to_string(), "some_get_val".to_string())
+            .unwrap();
+
         b.iter(|| {
-            store.get("key".to_string());
+            let _ = store.get("key".to_string()).unwrap();
         })
     });
+    group.finish();
+}
+fn set_many_keys(c: &mut Criterion) {
+
+    // let mut group = c.benchmark_group("Get Group");
+    // group.bench_function("kvs: get key", |b: &mut Bencher<_>| {
+    //     let temp_dir = TempDir::new().unwrap();
+    //     let mut store = KvStore::open(&temp_dir.path()).unwrap();
+    //     store.set("key".to_string(), "some_get_val".to_string()).unwrap();
+    //     b.iter(|| {
+    //         let _ = store.get("key".to_string()).unwrap();
+    //     })
+    // });
+    // group.bench_function("sled: get key", |b: &mut Bencher<_>| {
+    //     let temp_dir = TempDir::new().unwrap();
+    //     let mut store = SledKvsEngine::open(&temp_dir.path()).unwrap();
+    //     store.set("key".to_string(), "some_get_val".to_string()).unwrap();
+    //     b.iter(|| {
+    //         let _ = store.get("key".to_string()).unwrap();
+    //     })
+    // });
+    // group.finish();
 }
 
 // Define a criterion group `kv_benches` with the benchmarks under it
-criterion_group!(kv_benches, cold_start_get);
+criterion_group!(kv_benches, cold_start_get, set_many_keys);
 // Run all benchmarks in a given group
 criterion_main!(kv_benches);
